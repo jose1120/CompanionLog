315f5e282ec838f8417c73248162b61c56be848a
jose1120 <joseramirez25@my.whccd.edu>
2016-12-14 00:27:14 -0800

    third save

---
+++ public/javascripts/javascripts.js
@@ -0,0 +1,8 @@
+var newPet = document.getElementById('newpet');
+
+function newPetPage()
+{
+  location.href="/newpet";
+}
+
+newPet.addEventListener("click", newPetPage)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
+// mongoose connection
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/test');
+
+// schema (blueprint)
+var Schema = mongoose.Schema;
+
+var userDataSchema = new Schema({
+  name: String,
+  breed: String,
+  gender: String,
+  dob: Date,
+  color: String,
+  weight: Number
+});
+
+// Model (access to the collection)
+var UserData = mongoose.model('UserData', userDataSchema);
+
+// insert (CRUD)
+router.post('/insert', function(req,res,next){
+  var item = {
+    name: req.body.name,
+    breed: req.body.breed,
+    gender: req.body.gender,
+    dob: req.body.dob,
+    color: req.body.color,
+    weight: req.body.weight
+  };
+
+  var data = new UserData(item);
+
+  data.save();
+  res.redirect('/');
+});
+
+router.get('/get-data', function(req,res,next){
+  UserData.find()
+  .then(function(doc){
+    res.render('index', {items: doc});
+  });
+});
+
+router.get('/data', function(req,res,next){
+  UserData.find()
+    .then(function(doc){
+      res.render('data', {items: doc});
+    });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Companion Log' });
+});
+
+router.get('/newpet', function(req, res, next) {
+  res.render('newpet', { title: 'New Pet' });
+});
+
+router.get('/calendar', function(req, res, next) {
+  res.render('calendar', { title: 'Calendar' });
 });
 
 module.exports = router;
